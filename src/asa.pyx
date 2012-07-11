c8dc03a7e2d03269cb0aab1938e42836c6e94f7d
Robert Jordens <jordens@phys.ethz.ch>
2012-07-11 03:43:10 -0600

    cleanup long lines

--- src/asa.pyx
+++ src/asa.pyx
@@ -11,51 +11,63 @@ from asa_def cimport *
 np.import_array()
 
 asa_errors = {
+
     NORMAL_EXIT: """Given the criteria set, the
 search has run its normal course, e.g., when Limit_Acceptances or
 Limit_Generated is reached.""", 
+
     P_TEMP_TOO_SMALL: """A parameter temperature was too small using the
 set criteria.  Often this is an acceptable status code.""",
+
     C_TEMP_TOO_SMALL: """The cost temperature was too small using the set
 criteria.  Often this is an acceptable status code.""",
+
     COST_REPEATING: """The cost function value repeated a number of times
 using the set criteria. Often this is an acceptable status code.""",
+
     TOO_MANY_INVALID_STATES: """Too many repetitive generated states were
 invalid using the set criteria. This is helpful when using *cost_flag, as
 discussed above, to include constraints.""",
+
     IMMEDIATE_EXIT: """The user has set OPTIONS−>Immediate_Exit to TRUE, or
 deleted file asa_exit_anytime when ASA_EXIT_ANYTIME is TRUE.""",
+
     INVALID_USER_INPUT: """The user has introduced invalid input. When
 entering asa (), a function asa_test_asa_options () checks out many
 user−defined parameters and OPTIONS, and prints out invalid OPTIONS when
 ASA_PRINT is set to TRUE.""",
+
     INVALID_COST_FUNCTION: """The user has returned a value of the cost
 function to asa () which is not a valid number, e.g., not between
 -MAX_DOUBLE and MAX_DOUBLE.  Or, the user has returned a value of a
 parameter no longer within its proper range (excluding cases where the
 user has set the lower bound equal to the upper bound to remove a parameter
 from consideration).""",
+
     INVALID_COST_FUNCTION_DERIV: """While calculating numerical cost
-derivatives, a value of the cost function was returned which is not a valid
-number, e.g., not between -MAX_DOUBLE and MAX_DOUBLE. Or, while calculating
-numerical cost derivatives, a value of a parameter no longer within its
-proper range (excluding cases where the user has set the lower bound equal to
-the upper bound to remove a parameter from consideration) was set.  In such
-cases, review the bounds of parameters and the OPTIONS used to determine how
-derivatives are calculated and used.  When calculating derivatives, an
-invalid cost function, i.e., returning *cost_flag = FALSE from the cost
-function, will exit asa() with *exit_code
-INVALID_COST_FUNCTION_DERIV. Also, when calculating derivatives, no extra
-test is performed to check that parameters are within their lower and upper bounds (since meshes for derivatives may exceed these bounds). If this is not
-desired, then within the cost function, within a test for
-USER_OPTIONS->Locate_Cost == 3, a decision may be made whether to return this
-*exit_code.""",
+derivatives, a value of the cost function was returned which is not a
+valid number, e.g., not between -MAX_DOUBLE and MAX_DOUBLE. Or, while
+calculating numerical cost derivatives, a value of a parameter no longer
+within its proper range (excluding cases where the user has set the
+lower bound equal to the upper bound to remove a parameter from
+consideration) was set.  In such cases, review the bounds of parameters
+and the OPTIONS used to determine how derivatives are calculated and
+used.  When calculating derivatives, an invalid cost function, i.e.,
+returning *cost_flag = FALSE from the cost function, will exit asa()
+with *exit_code INVALID_COST_FUNCTION_DERIV. Also, when calculating
+derivatives, no extra test is performed to check that parameters are
+within their lower and upper bounds (since meshes for derivatives may
+exceed these bounds). If this is not desired, then within the cost
+function, within a test for USER_OPTIONS->Locate_Cost == 3, a decision
+may be made whether to return this *exit_code.""",
+
     CALLOC_FAILED: """Calloc memory allocation has failed in asa.c. This error
 will call Exit_ASA(), the location will be printed to stdout, and asa () will
 return the double -1 to the calling program. In asa_usr.c, if asa () returns
 this *exit_code a warning will be printed both to stdout and to USER_OUT.
 Note that if a calloc memory allocation fails in asa_usr.c, this error will
 call Exit_USER() to print the location to stdout and then return -2.""",
+
 }
 
 
